de21b1d5d1322365489d0623c6dce4026d4942dd
TZ <atian25@qq.com>
2022-10-02 11:21:45 +0800

    feat: first impl

--- index.js
+++ index.js
@@ -1,6 +1,77 @@
 'use strict';
 
-module.exports = async function run() {
-  return 'Hello World!';
+const Busboy = require('busboy');
+const { Queue } = require('@toolbuilder/await-for-it');
+const assert = require('assert/strict');
+const stream = require('stream');
+
+module.exports = function parser(request, options = {}) {
+  const queue = new Queue();
+
+  const parserOptions = extractOptions(request, options);
+
+  const busboy = Busboy(parserOptions);
+
+  busboy.on('field', onField);
+  busboy.on('file', onFile);
+  busboy.on('partsLimit', onLimit('partsLimit'));
+  busboy.on('filesLimit', onLimit('filesLimit'));
+  busboy.on('fieldsLimit', onLimit('fieldsLimit'));
+  // busboy.on('error', onError)
+  // busboy.on('close', onClose);
+
+  function onField(name, value, info) {
+    queue.push({ name, value, info });
+  }
+
+  function onFile(name, stream, info) {
+    // info.truncated = stream.truncated;
+    stream.once('limit', () => {
+      info.truncated = true;
+      stream.resume();
+      onLimit('fileSize')();
+    });
+    if (stream.truncated) {
+      stream.emit('limit');
+    }
+    queue.push({ name, stream, info });
+  }
+
+  function onLimit(type) {
+    return function onLimit() {
+      const error = new Error(`Reach ${type}`);
+      error.code = 'LIMIT_EXCEEDED';
+      error.status = 413;
+      onError(error);
+    };
+  }
+
+  function onClose() {
+    console.log('close');
+    queue.done();
+  }
+
+  function onError(err) {
+    queue.reject(err);
+  }
+
+  stream.pipeline(request, busboy, err => {
+    if (err) {
+      onError(err);
+    } else {
+      onClose();
+    }
+  });
+
+  return queue;
 };
 
+function extractOptions(request, options = {}) {
+  assert(request.headers, 'request.headers is required');
+
+  const headers = {};
+  for (const [ key, value ] of Object.entries(request.headers)) {
+    headers[key.toLowerCase()] = value;
+  }
+  return { ...options, headers };
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,54 @@
 const assert = require('assert/strict');
+const formstream = require('formstream');
+const stream = require('stream');
 
-const run = require('..');
+const parser = require('..');
 
 describe('test/index.test.js', () => {
   it('should work', async () => {
-    const result = await run();
-    assert.equal(result, 'Hello World!');
+    const form = formstream();
+    form.field('name', 'tz');
+    form.field('love', 'egg');
+    form.field('by', 'node.js');
+    form.field('by2', 'node.js');
+    form.file('file', __filename, 'test.js');
+    form.buffer('file2', Buffer.from('abc'), 'test2.js');
+    form.buffer('file3', Buffer.from('xxx'), '', 'application/octet-stream');
+
+    const request = new stream.PassThrough();
+    request.headers = form.headers();
+    form.pipe(request);
+
+    const parts = parser(request, {
+      limits: {
+        // fieldSize: 2,
+        // fields: 3,
+        // fileSize: 100,
+        // files: 1,
+      },
+    });
+
+    const { fields, files } = await collect(parts);
+    console.log(fields, files);
+    assert(fields.name.value === 'tz');
   });
 });
+
+
+async function collect(parts) {
+  const result = { fields: {}, files: {} };
+  for await (const part of parts) {
+    const { name, value, stream, info } = part;
+    if (!stream) {
+      result.fields[name] = { value, ...info };
+    } else {
+      let content = '';
+      for await (const chunk of stream) {
+        content += chunk.toString();
+      }
+      result.files[name] = { content, ...info };
+    }
+  }
+  return result;
+}
+
